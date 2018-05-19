9009ebb37be3298b9261871d3d1a74fb7d364c3e
Stephan14 <zsf657135507@gmail.com>
2018-05-19 12:51:46 +0000

    Thread

---
+++ src/Thread.cc
@@ -0,0 +1,49 @@
+#include "Thread.h"  
+  
+#include <assert.h>
+  
+Thread::Thread()  
+{  
+    pid_ = 0;  
+    state_ = THREAD_STATUS_NEW;  
+}  
+  
+bool Thread::Start()  
+{  
+    if (pthread_create(&pid_, nullptr, ThreadProxyFunc, this) == 0) {
+        state_ = THREAD_STATUS_RUNNING;
+        return true;
+    } else {
+        return false;
+    }
+}  
+  
+pthread_t Thread::GetThreadID()  
+{  
+    return pid_;  
+}  
+  
+int Thread::GetState()  
+{  
+    return state_;  
+}  
+  
+void Thread::Join()  
+{  
+    if (pid_ > 0)  
+    {  
+        pthread_join(pid_, nullptr);  
+    }  
+}  
+
+void * Thread::ThreadProxyFunc(void * args)  
+{  
+    Thread * pthread = static_cast<Thread *>(args);   
+    assert(pthread != nullptr);
+    pthread->Run();   
+    return nullptr;   
+}  
+  
+Thread::~Thread()
+{
+}
---
+++ src/Thread.h
@@ -0,0 +1,40 @@
+#ifndef THREAD_H  
+#define THREAD_H  
+#include <iostream>  
+#include <pthread.h>  
+  
+  
+class Thread  
+{  
+public:  
+    enum ThreadState {
+        THREAD_STATUS_NEW,
+        THREAD_STATUS_RUNNING,
+        THREAD_STATUS_EXIT
+    };
+    
+    //构造函数  
+    Thread();  
+    //析构函数
+    virtual ~Thread();
+    //线程的运行实体  
+    virtual void Run()=0;  
+    //开始执行线程  
+    bool Start();  
+    //获取线程ID  
+    pthread_t GetThreadID();  
+    //获取线程状态  
+    int GetState();  
+    //等待线程直至退出  
+    void Join();  
+
+    private:  
+    //当前线程的线程ID  
+    pthread_t pid_;  
+    //线程的状态  
+    ThreadState state_;  
+    //获取执行方法的指针  
+    static void * ThreadProxyFunc(void * args);  
+};  
+  
+#endif  

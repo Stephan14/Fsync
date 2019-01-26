674c06cc9ddd3a82f0589142669fe0664d84d2dc
Stephan14 <zsf657135507@gmail.com>
2019-01-26 22:57:10 +0800

    update metadata

--- src/MetaData.cc
+++ src/MetaData.cc
@@ -16,59 +16,31 @@ MetaData::~MetaData()
 {
 }
 
-void MetaData::PutNewFile(std::string file_path)
+void MetaData::AddOrUpdateFileMsg(const std::string &filePath, const std::string &hash, const std::string &compressFile)
 {
-    if (file_path == "")
+    if (filePath == "")
         return;
     mutex.lock();
-    time_t current_time = getCurrentTime();
-    undone_file[file_path] = current_time;
+    FileAndHash[filePath] = hash;
+    FileAndCompress[filePath] = compressFile;
     mutex.unlock();
 }
 
-bool Cmp(const std::pair<std::string, std::time_t> &v1, const std::pair<std::string, std::time_t> &v2)
+std::map<std::string, std::string> MetaData::GetFileMsg()
 {
-    return v1.second < v2.second;
-}
-
-std::string MetaData::GetNewFileByTime()
-{
-    std::string file_path = "";
-
+    std::map<std::string, std::string> res;
     mutex.lock();
-    std::vector<std::pair<std::string, std::time_t>> arr;
-    for (std::map<std::string, std::time_t>::iterator it = undone_file.begin(); it != undone_file.end(); ++it)
-    {
-        arr.push_back(make_pair(it->first, it->second));
-    }
-    sort(arr.begin(), arr.end(), Cmp);
-
-    if (arr.size() > 0) {
-        undone_file.erase(arr[0].first);
-        file_path = arr[0].first;
-    }
-
+    res = FileAndHash;
     mutex.unlock();
-    return file_path;
+    return res;
 }
 
-void MetaData::StartCompressFile(std::string file_path, pthread_t threadID)
+std::map<std::string, std::string> MetaData::GetFileCompressMsg()
 {
-    doing_file[file_path] = threadID;
+    std::map<std::string, std::string> res;
+    mutex.lock();
+    res = FileAndCompress;
+    mutex.unlock();
+    return res;
 }
-
-void MetaData::CompleteCompressFile(std::string file_path, std::string compressedFile)
-{
-    auto threadID = doing_file.find(file_path);
-    if (threadID == doing_file.end()) {
-        return;
-    }
-
-    doing_file.erase(file_path);
-
-    done_file[file_path] = compressedFile;
-}
-
-
-
 
--- src/MetaData.h
+++ src/MetaData.h
@@ -21,15 +21,13 @@ class MetaData
         MetaData();
         ~MetaData();
 
-        void PutNewFile(std::string file_path);
-        std::string GetNewFileByTime();
-        void StartCompressFile(std::string file_path, pthread_t threadId);
-        void CompleteCompressFile(std::string file_path, std::string compressedFile);
+        void AddOrUpdateFileMsg(const std::string &file_path, const std::string &hash, const std::string &current_time);
+        std::map<std::string, std::string> GetFileMsg();
 
+        std::map<std::string, std::string> GetFileCompressMsg();
     private:
-        std::map<std::string, std::string> done_file;
-        std::map<std::string, std::time_t> undone_file;
-        std::map<std::string, pthread_t> doing_file;
+        std::map<std::string, std::string> FileAndHash;
+        std::map<std::string, std::string> FileAndCompress;
         std::mutex mutex; // for undone_file
 
 };
